ccaa692670c0ae6f07fdc3e559fdef74065501c1
khamitovdr <khamitovdr@yandex-team.ru>
2024-12-14 11:44:26 +0300

    Add Relation and Condition classes for conditional action evaluation

--- src/bioexperiment_suite/experiment/__init__.py
+++ src/bioexperiment_suite/experiment/__init__.py
@@ -1,3 +1,5 @@
 from .experiment import Experiment
+from .conditional_actions import Condition, Relation
+from .metrics import Statistic, Metric
 
-__all__ = ["Experiment"]
+__all__ = ["Experiment", "Condition", "Statistic", "Relation", "Metric"]
---
+++ src/bioexperiment_suite/experiment/conditional_actions.py
@@ -0,0 +1,67 @@
+from typing import Callable, TypeAlias
+from bioexperiment_suite.experiment import Metric
+
+
+RelationFunction: TypeAlias = Callable[[float], bool]
+
+
+class Relation:
+    """Class to define common relations for conditional actions."""
+
+    @staticmethod
+    def equals_to(value: float) -> RelationFunction:
+        """Returns a relation checking if a metric equals the given value."""
+        return lambda x: x == value
+
+    @staticmethod
+    def greater_than(value: float) -> RelationFunction:
+        """Returns a relation checking if a metric is greater than the given value."""
+        return lambda x: x > value
+
+    @staticmethod
+    def less_than(value: float) -> RelationFunction:
+        """Returns a relation checking if a metric is less than the given value."""
+        return lambda x: x < value
+
+    @staticmethod
+    def greater_than_or_equals_to(value: float) -> RelationFunction:
+        """Returns a relation checking if a metric is greater than or equal to the given value."""
+        return lambda x: x >= value
+
+    @staticmethod
+    def less_than_or_equals_to(value: float) -> RelationFunction:
+        """Returns a relation checking if a metric is less than or equal to the given value."""
+        return lambda x: x <= value
+
+    @staticmethod
+    def not_equals_to(value: float) -> RelationFunction:
+        """Returns a relation checking if a metric does not equal the given value."""
+        return lambda x: x != value
+
+
+class Condition:
+    """Class to define a condition to determine if an action should be executed."""
+
+    def __init__(self, metric: Metric, relation: RelationFunction):
+        """
+        Initialize the condition for the action to be executed.
+
+        :param metric: The metric to evaluate.
+        :param relation: A callable that takes the metric's value and returns a boolean.
+        """
+        if not isinstance(metric, Metric):
+            raise TypeError("Expected a Metric instance for 'metric'.")
+        if not callable(relation):
+            raise TypeError("Expected a callable for 'relation'.")
+
+        self.metric = metric
+        self.relation = relation
+
+    def check_condition(self) -> bool:
+        """
+        Check if the condition is met.
+
+        :return: True if the condition is met, False otherwise.
+        """
+        metric_value = self.metric.get_value()
+        return self.relation(metric_value)
