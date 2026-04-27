31da72fab1607bcf09a03e0a623f1b58dbc859ec
khamitovdr <khamitovdr@yandex-team.ru>
2026-04-27 18:37:42 +0300

    Update experiment_example.py to use LabDevicesClient

--- examples/experiment_example.py
+++ examples/experiment_example.py
@@ -1,5 +1,5 @@
 from bioexperiment_suite.experiment import Experiment  # Import the Experiment class
-from bioexperiment_suite.tools import get_connected_devices  # Import the get_connected_devices function
+from bioexperiment_suite.interfaces import LabDevicesClient
 
 # Define the experiment parameters
 TOTAL_EXPERIMENT_DURATION_HOURS = 24  # Total duration of the experiment in hours
@@ -9,6 +9,8 @@ MEASUREMENT_INTERVAL_MINUTES = 5  # Interval for taking measurements in minutes
 POURED_OUT_VOLUME_ML = 2  # Volume of solution poured out in mL
 INFUSED_VOLUME_ML = 1  # Volume of solution infused in mL
 FLOW_RATE_ML_PER_MINUTE = 3  # Flow rate of the pump in mL/min
+
+LAB_DEVICES_PORT = 9001  # Per-lab-machine chisel-tunnel port
 
 # Ensure intervals are valid
 assert (
@@ -21,40 +23,40 @@ assert (
 # Calculate the number of solution refreshes and measurements per refresh
 n_solution_refreshes = (
     TOTAL_EXPERIMENT_DURATION_HOURS * 60 // SOLUTION_REFRESH_INTERVAL_MIN
-)  # Number of solution refreshes
+)
 n_measurements_per_solution_refresh = (
     SOLUTION_REFRESH_INTERVAL_MIN // MEASUREMENT_INTERVAL_MINUTES
-)  # Number of measurements per refresh
+)
 
-# Retrieve connected devices by checking the serial ports
-pumps, spectrophotometers = get_connected_devices()
+# Connect to the lab devices service and discover devices
+client = LabDevicesClient(port=LAB_DEVICES_PORT)
+devices = client.discover()
 
 # Unpack discovered devices
-(pump1, pump2) = pumps  # Suppose we have two pumps
-(spectrophotometer,) = spectrophotometers  # Suppose we have one spectrophotometer
+(pump1, pump2) = devices.pumps  # Suppose we have two pumps
+(densitometer,) = devices.densitometers  # Suppose we have one densitometer
 
 # Initialize the experiment
 experiment = Experiment()
 
 # Configure the experiment with measurements and actions
-for i in range(n_solution_refreshes):  # Loop over the number of solution refreshes
-    for j in range(n_measurements_per_solution_refresh):  # Loop over the number of measurements per refresh
+for i in range(n_solution_refreshes):
+    for j in range(n_measurements_per_solution_refresh):
         experiment.add_measurement(
-            spectrophotometer.get_temperature, measurement_name="Temperature (C)"
-        )  # Measure temperature
+            densitometer.get_temperature, measurement_name="Temperature (C)"
+        )
         experiment.add_measurement(
-            spectrophotometer.measure_optical_density, measurement_name="Optical density"
-        )  # Measure optical density
-        experiment.add_wait(MEASUREMENT_INTERVAL_MINUTES * 60)  # Wait for the measurement interval
+            densitometer.measure_optical_density, measurement_name="Optical density"
+        )
+        experiment.add_wait(MEASUREMENT_INTERVAL_MINUTES * 60)
 
-    # Add actions to refresh the solution
     experiment.add_action(
         pump2.pour_in_volume, volume=POURED_OUT_VOLUME_ML, flow_rate=FLOW_RATE_ML_PER_MINUTE, direction="left"
-    )  # Pour out the old solution
+    )
     experiment.add_action(
         pump1.pour_in_volume, volume=INFUSED_VOLUME_ML, flow_rate=FLOW_RATE_ML_PER_MINUTE, direction="right"
-    )  # Infuse the new solution
-    experiment.add_wait(MEASUREMENT_INTERVAL_MINUTES * 60)  # Wait after refreshing the solution
+    )
+    experiment.add_wait(MEASUREMENT_INTERVAL_MINUTES * 60)
 
 # Run the experiment
 experiment.start()
